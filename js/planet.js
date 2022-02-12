403427f310a82ecb273edb2e0d2ad8734e434883
lorossi <contact@lorenzoros.si>
2022-02-12 23:06:11 +0100

    file renamed

--- js/planet.js
+++ js/planet.js
@@ -1,10 +1,17 @@
-class Galaxy {
+class Planet {
   constructor(x, y, size) {
+    // top left corner position of the galaxy
     this._x = x;
     this._y = y;
+    // size of the galaxy
     this._size = size;
 
+    // border of the rectangle containing the galaxy
+    this._border = 0.25;
+
     // TODO pick better colours
+
+    // fancy array shuffling by 1loc.com
     this._planet_colours = [
       "#e96d5e",
       "#ff9760",
@@ -25,12 +32,12 @@ class Galaxy {
 
     this._fill = this._planet_colours.pop();
 
-    this._moons_number = random_int(1, 4);
-    this._border = 0.25;
+    this._moons_number = random_int(2, 6);
 
+    // planet radius
     this._r = (this._size / 2) * (1 - this._border);
-    this._theta = Math.random() * Math.PI * 2;
 
+    // moons!
     this._moons = [];
     this._generateMoons();
   }
@@ -46,16 +53,16 @@ class Galaxy {
     ctx.save();
 
     ctx.translate(this._size / 2, this._size / 2);
-    ctx.rotate(this._theta);
 
+    // draw the main planet
     ctx.fillStyle = this._fill;
-
     ctx.beginPath();
     ctx.arc(0, 0, this._r, 0, Math.PI * 2);
     ctx.fill();
 
     ctx.restore();
 
+    // draw the moons
     this._moons.forEach((m) => {
       ctx.save();
       ctx.translate(this._size / 2, this._size / 2);
@@ -67,41 +74,51 @@ class Galaxy {
     ctx.restore();
   }
 
+  // generate some moons for this planet
   _generateMoons() {
     for (let i = 0; i < this._moons_number; i++) {
       this._moons.push(
-        new Planet(this._size * (1 - this._border), this._planet_colours.pop())
+        new Moon(this._size * (1 - this._border), this._planet_colours.pop())
       );
     }
   }
 }
 
-class Planet {
+class Moon {
   constructor(size, color) {
-    const scl_factor = random_int(10, 30);
+    // ratio between planet and moon size
+    const scl_factor = random_int(5, 40);
     this._r = size / scl_factor;
+    // some approximation for the orbit size
     this._orbit_size = size + 2 * this._r;
     this._fill = color;
-
+    // when should the planet start appearing or disappearing?
     this._disappearing_edge = random(0.05, 0.1);
 
     this._theta = Math.random() * Math.PI * 2;
     this._start_x = Math.random();
     this._x = 0;
     this._hidden = true;
+    // number of orbits in the total animation duration
     this._orbits = random_int(1, 3);
+
+    // TODO texturize with some craters
   }
 
   update(percent) {
+    // calculate actual orbit position
     const orbit_pos = ((this._start_x + percent) * this._orbits) % 1;
-    this._hidden = orbit_pos > 0.5;
 
+    // check if the moon is behind the planet
+    this._hidden = orbit_pos > 0.5;
     if (this._hidden) return;
 
+    // actual percent
     const real_percent = (orbit_pos * 2) % 1;
-
+    // x position along the orbit (will then be rotated)
     this._x = real_percent * this._orbit_size - this._orbit_size / 2;
 
+    // calculation to shrink/enlarge the moon when close to the planet's border
     if (real_percent < this._disappearing_edge) {
       const x = real_percent / this._disappearing_edge; // [0, 0.05] -> [0, 1]
       this._current_r = this._easeIn(x) * this._r;
@@ -118,7 +135,7 @@ class Planet {
     ctx.translate(this._x, 0);
 
     ctx.fillStyle = this._fill;
-    ctx.strokeStyle = "rgba(220, 220, 220, 0.5)";
+    ctx.strokeStyle = "rgba(220, 220, 220, 0.5)"; // TODO improve this
     ctx.beginPath();
     ctx.arc(0, 0, this._current_r, 0, Math.PI * 2);
     ctx.fill();
@@ -135,6 +152,7 @@ class Planet {
     return Math.sin((x * Math.PI) / 2);
   }
 
+  // moon orbit rotation
   get theta() {
     return this._theta;
   }
